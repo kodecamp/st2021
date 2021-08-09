6830ac94a80482bc99b76e10ebe2a22bcf11200b
kodecamp <kodecamp.in@gmail.com>
2021-08-09 13:41:10 +0530

    Updated

---
+++ code/src/Student.java
@@ -0,0 +1,28 @@
+class Student {
+  String rollNo;
+
+  // constructor
+  Student(String rollNo) {
+    this.rollNo = rollNo;
+  }
+
+  String adharNo() {
+    return null;
+  }
+
+  String name() {
+    return null;
+  }
+
+  String address() {
+    return null;
+  }
+
+  String college() {
+    return null;
+  }
+
+  String rollNo() {
+    return null;
+  }
+}
---
+++ code/src/Test.java
@@ -0,0 +1,8 @@
+class Test {
+
+  public static void main(String... args) {
+    Person p1 = new Person(100);
+    int value = p1.adharNo;
+    System.out.println("Value : " + value);
+  }
+}
